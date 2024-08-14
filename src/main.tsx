d5378adf4c7302ef84691003334f7f112322d3ea
Elena Kolesnikova <helena_uni@live.ru>
2024-08-14 21:38:59 +0200

    fixed eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {
-    BrowserRouter, Routes, Route,
+  BrowserRouter, Routes, Route,
 } from 'react-router-dom';
 import './index.css';
-import AuthProvider from './Context/Context';
-import {SignIn} from './SignIn/SignIn';
-import {SignUp} from './SignUp/SignUp';
-import { Nannies } from './NanniesList/NanniesList';
-import { NannyDetails } from './NannyDetails/NannyDetails';
-import { Profile } from './Profile/Profile';
+import AuthProvider from './Context/Context.tsx';
+import {SignIn} from './pages/SignIn/SignIn';
+import {SignUp} from './pages/SignUp/SignUp.jsx';
+import { Nannies } from './pages/NanniesList/NanniesList.jsx';
+import { NannyDetails } from './pages/NannyDetails/NannyDetails.jsx';
+import { Profile } from './pages/Profile/Profile.jsx';
 import VerifyCode from './VerifyCode/VerifyCode';
-import Header from './Header/Header';
-import { Footer } from './Footer/Footer';
+import Header from './components/Header/Header.tsx';
+import { Footer } from './components/Footer/Footer.jsx';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const defaultTheme = createTheme() ;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <BrowserRouter>
-          <AuthProvider>
-              <ThemeProvider theme={defaultTheme}>
-                  <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <div>
-                      <Header />
-                      <Routes>
-                          <Route path="/" element={<Nannies />} />
-                          <Route path="/:id" element={<NannyDetails />} />
-                          <Route path="/login" element={<SignIn />} />
-                          <Route path="/signup" element={<SignUp />} />
-                          <Route path="/profile" element={<Profile />} />
-                          <Route path="/verify-code/:code" element={<VerifyCode />} />
-                      </Routes>
-                      <Footer/>
-                  </div>
-                  </LocalizationProvider>
-              </ThemeProvider>
-          </AuthProvider>
-      </BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
+        <ThemeProvider theme={defaultTheme}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <div>
+              <Header />
+              <Routes>
+                <Route path="/" element={<Nannies />} />
+                <Route path="/:id" element={<NannyDetails />} />
+                <Route path="/login" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/verify-code/:code" element={<VerifyCode />} />
+              </Routes>
+              <Footer/>
+            </div>
+          </LocalizationProvider>
+        </ThemeProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
